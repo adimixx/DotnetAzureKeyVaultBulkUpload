dd4a0e9f93410126e849dcb41c512b89664fc34e
adimixx <adimixxdev@gmail.com>
2024-10-21 11:29:52 +0800

    Added Console Readline, added operations for Importing KVs

--- Program.cs
+++ Program.cs
@@ -1,50 +1,89 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using System.Text;
+﻿using System.Text;
 using Azure.Identity;
 using Azure.Security.KeyVault.Secrets;
 
-
-
 var azureKeyVaultUri = "";
+var operation = 0;
+while (string.IsNullOrWhiteSpace(azureKeyVaultUri))
+{
+    Console.Write("Enter your Azure Key Vault URI: ");
+    azureKeyVaultUri = Console.ReadLine();
+
+    if (string.IsNullOrWhiteSpace(azureKeyVaultUri))
+    {
+        Console.Write("Invalid input given. Please try again");
+    }
+}
+
 var secretClient = new SecretClient(new Uri(azureKeyVaultUri), new DefaultAzureCredential());
 
-// Get all Secret keys from KV
-var secretProps = secretClient.GetPropertiesOfSecrets();
-var stringBuilder = new StringBuilder();
-foreach (var secretProp in secretProps)
+while (operation != 1 && operation != 2)
 {
-    stringBuilder.AppendLine(secretProp.Name);
+    Console.WriteLine("1 : Export Secrets");
+    Console.WriteLine("2 : Bulk Import Secrets");
+    
+    Console.Write("Please Enter Operation : ");
+    var input = Console.ReadLine();
+
+    int.TryParse(input, out operation);
 }
 
-// Export file to csv
-var outputDir = Path.Combine(AppContext.BaseDirectory, "Outputs");
-
-if (!Directory.Exists(outputDir))
+if (operation == 1)
 {
-    Directory.CreateDirectory(outputDir);
+    // Get all Secret keys from KV
+    var secretProps = secretClient.GetPropertiesOfSecrets();
+    var stringBuilder = new StringBuilder();
+    foreach (var secretProp in secretProps)
+    {
+        stringBuilder.AppendLine(secretProp.Name);
+    }
+    Console.WriteLine($"{secretProps.Count()} secrets have been exported from Azure Key Vault. Writing to the file.");
+
+    var outputDir = Path.Combine(AppContext.BaseDirectory, "Outputs");
+    if (!Directory.Exists(outputDir))
+    {
+        Directory.CreateDirectory(outputDir);
+    }
+
+    // Export file to csv
+    File.WriteAllText(Path.Combine(outputDir, "SecretKeys.csv"), stringBuilder.ToString());
+    Console.WriteLine("Success!");
 }
 
-File.WriteAllText(Path.Combine(outputDir, "SecretKeys.csv"), stringBuilder.ToString());
+else
+{
+    var inputDir = Path.Combine(AppContext.BaseDirectory, "Inputs");
+    var inputFile = Path.Combine(inputDir, "SecretsToUpload.csv");
+    if (!File.Exists(inputFile))
+    {
+        throw new FileNotFoundException("File not found", inputFile);
+    }
 
-//
-// var streamReader = new StreamReader("file.csv");
-// var keyVals = new List<KeyValuePair<string, string>>();
-//
-// while (!streamReader.EndOfStream)
-// {
-//     var splits = streamReader.ReadLine().Split(';');
-//     keyVals.Add(new KeyValuePair<string, string>(splits[0].Trim(), splits[1].Trim()));
-// }
-//
-// Console.WriteLine($"No of secrets: {keyVals.Count}");
-//
-//
-//
-//
-// foreach (var kv in keyVals)
-// {
-//     await secretClient.SetSecretAsync(kv.Key, kv.Value);
-//     Console.WriteLine($"Secret sync to KV : {kv.Key}");
-// }
-//
+    var streamReader = new StreamReader(inputFile);
+    var keyVals = new List<KeyValuePair<string, string>>();
+
+    while (!streamReader.EndOfStream)
+    {
+        var splits = streamReader.ReadLine().Split(';');
+        if (splits.Length >= 2 && !string.IsNullOrWhiteSpace(splits[0].Trim()) && !string.IsNullOrWhiteSpace(splits[1].Trim()))
+        {
+            keyVals.Add(new KeyValuePair<string, string>(splits[0].Trim(), splits[1].Trim()));
+            Console.WriteLine($"Secret Added To List : {splits[0].Trim()} - {splits[1].Trim()}");
+        }
+    }
+
+    Console.WriteLine($"No of secrets: {keyVals.Count}");
+
+    if (!keyVals.Any())
+    {
+        Console.WriteLine("No of secrets have been imported.");
+        return;
+    }
+
+    foreach (var kv in keyVals)
+    {
+        await secretClient.SetSecretAsync(kv.Key, kv.Value);
+        Console.WriteLine($"Secret synced to KV : {kv.Key}");
+    }
+}
+
